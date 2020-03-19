1a4da13a963437852c454982ac11f5f939b97c2d
tobias-fyi <tobyreaper@gmail.com>
2020-03-19 12:16:07 -0600

    End of Algorithms lectures

--- Algorithms/notes/cookies_brian.py
+++ Algorithms/notes/cookies_brian.py
@@ -8,19 +8,49 @@ Implement a function `eating_cookies` that counts the number of
 possible ways Cookie Monster can eat all of the cookies in the jar.
 """
 
+# %%
 import sys
 
-# n = 6
-# [ 1 1 1 1 1 1 ] +1
-# [ 1 1 1 1 2 - ] +1
-# [ 1 1 2 - 2 - ] +1
-# [ 2 - 2 - 2 - ] +1
-# [ 1 1 1 3 - - ] +1
-# [ 3 - - 3 - - ] +1
-# [ 1 2 - 3 - - ] +1
+# n = 5
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive recursive solution
+#     1 1 1 1 1
+#     1 1 1 2
+#     1 1 2 1
+#     1 2 1 1
+#     2 1 1 1
+#     1 1 3
+#     1 3 1
+#     3 1 1
+#     1 2 2
+#     2 1 2
+#     2 2 1
+#     2 3
+#     3 2
+
+
+# %%
+# def eating_cookies(n: int, cache=None) -> int:
+#     # Base cases
+#     if n == 0:
+#         return 1
+#     if n == 1:
+#         return 1
+#     if n == 2:
+#         return 2
+#     if n == 3:
+#         return 4
+
+#     ec1 = eating_cookies(n - 1)
+#     ec2 = eating_cookies(n - 2)
+#     ec3 = eating_cookies(n - 3)
+
+#     return ec1 + ec2 + ec3
+
+
+# eating_cookies(9)
+
+# %%
+# With cache
 def eating_cookies(n: int, cache=None) -> int:
     if cache is None:
         cache = {}
@@ -45,11 +75,7 @@ def eating_cookies(n: int, cache=None) -> int:
     return cache[n]
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        num_cookies = int(sys.argv[1])
-        print(f"There are {eating_cookies(num_cookies)} ways for")
-        print(f"Cookie Monster to eat {num_cookies} cookies.")
-    else:
-        print("Usage: eating_cookies.py [num_cookies]")
+eating_cookies(15)
+
+# %%
 
